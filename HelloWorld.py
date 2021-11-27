ffe49a69c2a3352044a296543b6c8bb44fbafe21
junedalishaik95 <junedali.shaik@gmail.com>
2021-11-27 21:10:15 +0530

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,35 @@
+print(2+3)
+
+print("Hell0")
+
+a = -1
+
+b = "cool"
+
+c = 71.22
+
+d = True
+
+e = None
+
+# Print the variable
+
+print(a)
+
+print(b)
+
+print(c)
+
+print(d)
+
+print(e)
+
+print(type(a))
+
+print(type(b))
+
+print(type(c))
+
+print(type(d))
+
+print(type(e))
